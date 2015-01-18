9176149133fb2d97d7cf2dfc0e273aca6b08483e
Spencer Carli <spencercarli@gmail.com>
2015-01-18 18:43:29 -0500

    Put elements in place on homescreen

--- both/collections/photos.js
+++ both/collections/photos.js
@@ -1,7 +1,13 @@
 Photos = new Mongo.Collection('photos');
 
 Photos.helpers({
+  owner: function() {
+    return Users.findOne({_id: this.ownerId});
+  },
 
+  timeAgo: function() {
+    return moment(this.createdAt).fromNow(true);
+  }
 });
 
 Photos.before.insert(function(userId, doc){
--- client/templates/home/home.js
+++ client/templates/home/home.js
@@ -1,3 +1,22 @@
 Template.home.rendered = function() {
 
 };
+
+Template.home.events({
+  'click [data-action=like]': function(e, tmp) {
+    alert('Not Implement Yet');
+  },
+
+  'dblclick [data-action=like-photo]': function(e, tmp) {
+    alert('Not Implement Yet');
+  },
+
+  'click [data-action=comment]': function(e, tmp) {
+    alert('Not Implement Yet');
+  },
+
+  'click [data-action=more]': function(e, tmp) {
+    alert('Not Implement Yet');
+  },
+
+});
